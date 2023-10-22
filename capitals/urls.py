edb863c0754f30daca9ad588f874ae424223ed77
Leo Suarez <leo@dtraleigh.com>
2023-10-22 13:59:11 -0400

    Cleaning up the urls with a trailing slash.

--- capitals/urls.py
+++ capitals/urls.py
@@ -6,5 +6,5 @@ app_name = "capitals"
 urlpatterns = [
     path("", views.home, name="home"),
     path("map/", views.map_page, name="map_page"),
-    path("capital/<str:cap_name>", views.capital_page, name="capital_page"),
+    path("capital/<str:cap_name>/", views.capital_page, name="capital_page"),
 ]
--- videos/urls.py
+++ videos/urls.py
@@ -4,17 +4,17 @@ from videos import views
 
 app_name = "videos"
 urlpatterns = [
-    path(r"", views.video_login),
+    path("", views.video_login),
     path("main/", views.main),
     path("map/", views.map_view),
     path("main/upload/", views.upload),
     path("random/", views.random_shot_view),
     path("logout/", views.video_logout),
-    path("album/<int:album_id>", views.album_view, name="album_view"),
-    path("album/<int:album_id>/<str:shot_type>/<int:shot_id>", views.shot_view),
-    path("tag/<str:tag_name>/<str:shot_type>/<int:shot_id>", views.video_tag_view),
-    path("tag/<str:tag_name>", views.tag_view),
-    path("recent/<str:shot_type>/<int:shot_id>", views.recent_view, name="recent_shot"),
-    path("edit/<str:shot_type>/<int:shot_id>", views.shot_edit_view),
-    path("map/<str:shot_type>/<int:shot_id>", views.map_shot),
+    path("album/<int:album_id>/", views.album_view, name="album_view"),
+    path("album/<int:album_id>/<str:shot_type>/<int:shot_id>/", views.shot_view),
+    path("tag/<str:tag_name>/<str:shot_type>/<int:shot_id>/", views.video_tag_view),
+    path("tag/<str:tag_name>/", views.tag_view),
+    path("recent/<str:shot_type>/<int:shot_id>/", views.recent_view, name="recent_shot"),
+    path("edit/<str:shot_type>/<int:shot_id>/", views.shot_edit_view),
+    path("map/<str:shot_type>/<int:shot_id>/", views.map_shot),
 ]
