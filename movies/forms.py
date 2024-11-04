da2ac5420be0f765b34ca6d6e4461f5bb8b8f5b8
unknown <leo@dtraleigh.com>
2024-11-03 20:02:47 -0500

    Updates to avoid querying the database before new migrations take place.

--- movies/forms.py
+++ movies/forms.py
@@ -1,23 +1,16 @@
 from django import forms
-
 from movies.models import Format, Collection
-
-FORMATS = []
-for movie_format in [f for f in Format.objects.all()]:
-    FORMATS.append((movie_format.name, movie_format.name))
-
-COLLECTIONS = []
-for collection in [c for c in Collection.objects.all()]:
-    COLLECTIONS.append((collection.name, collection.name))
-
 
 class AddMovieForm(forms.Form):
     imdb_id = forms.CharField(label="IMDB ID", max_length=100)
-    formats = forms.MultipleChoiceField(label="Formats", choices=FORMATS, widget=forms.CheckboxSelectMultiple())
+    formats = forms.MultipleChoiceField(label="Formats", choices=[], widget=forms.CheckboxSelectMultiple())
     is_tv_movie = forms.BooleanField(label="TV Movie?", required=False)
     letterboxd_slug = forms.CharField(label="Letterboxd slug", max_length=200)
     sort_title = forms.CharField(label="Sort Title", max_length=50, required=False)
-    collections = forms.MultipleChoiceField(label="Collections",
-                                            required=False,
-                                            choices=COLLECTIONS,
-                                            widget=forms.CheckboxSelectMultiple())
+    collections = forms.MultipleChoiceField(label="Collections", choices=[], required=False, widget=forms.CheckboxSelectMultiple())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Populate the choices only when the form is initialized
+        self.fields['formats'].choices = [(f.name, f.name) for f in Format.objects.all()]
+        self.fields['collections'].choices = [(c.name, c.name) for c in Collection.objects.all()]
